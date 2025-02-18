d7d676323975cf274107f768114938a6aefb6a65
faresarar <faresarar455@gmail.com>
2025-02-18 15:57:20 +0100

    Adding Semantics and SafeArea

--- lib/widgets/location_selection.dart
+++ lib/widgets/location_selection.dart
@@ -6,38 +6,43 @@ class LocationSelection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size sizeScreen = MediaQuery.sizeOf(context);
-    return InkWell(
-      onTap:(){},
-      child: Container(
-        color: Colors.redAccent,
-        padding: EdgeInsets.symmetric(horizontal: sizeScreen.width * 0.041),
-        height: 62,
-        child: Row(
-          spacing: 12,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Icon(
-              Icons.flag,
-              color: Colors.white,
-              size: 36,
+    return SafeArea(
+      child: Semantics(
+        button: true,
+        child: InkWell(
+          onTap:(){},
+          child: Container(
+            color: Colors.redAccent,
+            padding: EdgeInsets.symmetric(horizontal: sizeScreen.width * 0.041),
+            height: 62,
+            child: Row(
+              spacing: 12,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(
+                  Icons.flag,
+                  color: Colors.white,
+                  size: 36,
+                ),
+                Text(
+                  "Select Country / Location",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.arrow_right,
+                    color: Colors.redAccent,
+                    size: 26,
+                  ),
+                )
+              ],
             ),
-            Text(
-              "Select Country / Location",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            CircleAvatar(
-              backgroundColor: Colors.white,
-              child: Icon(
-                Icons.arrow_right,
-                color: Colors.redAccent,
-                size: 26,
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
