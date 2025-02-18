11dc8e9ac3864ede038a881389e1849fc862957d
faresarar <faresarar455@gmail.com>
2025-02-18 15:50:31 +0100

    Adding bottomNavigationBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import '../App Preferences/app_preferences.dart';
-
+import '../widgets/location_selection.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: LocationSelection(),
       appBar: AppBar(
         title: Text("Free Vpn"),
         leading: IconButton(
