722892a74d7e44c586caebc0b9a19af07fee6f53
faresarar <faresarar455@gmail.com>
2025-02-18 19:15:44 +0100

    save OpenVpn Server Details

--- lib/App Preferences/app_preferences.dart
+++ lib/App Preferences/app_preferences.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
+
+import '../models/vpn_info_model.dart';
 
 class AppPreferences {
   static late Box boxOfData;
@@ -11,4 +14,23 @@ class AppPreferences {
   /// saving user choice about theme selection
   static bool get isDarkMode => boxOfData.get("isDarkMode") ?? false;
   static set isDarkMode(bool value) => boxOfData.put("isDarkMode", value);
+
+  /// for saving single selected vpn details
+
+  static VpnInfoModel get vpnInfoModelObject => VpnInfoModel.fromJson(
+        boxOfData.get("vpn" ?? "{}"),
+      );
+  static set vpnInfoModelObject(VpnInfoModel value) =>
+      boxOfData.put("vpn", jsonEncode(value));
+  static List<VpnInfoModel> get vpnList {
+    List<VpnInfoModel> tempVpnList = [];
+    final dataVpn = jsonDecode(boxOfData.get("vpnList" ?? "[]"));
+    for (var data in dataVpn) {
+      tempVpnList.add(VpnInfoModel.fromJson(dataVpn));
+    }
+    return tempVpnList;
+  }
+
+  static set vpnList(List<VpnInfoModel> valueList) =>
+      boxOfData.put("vpnList", jsonEncode(valueList));
 }
